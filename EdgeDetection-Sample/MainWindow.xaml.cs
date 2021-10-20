3e1c9fc555ece17b397171124c51b6ca505e5b74
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-10-20 21:42:09 +0800

    Update MainWindow.xaml.cs

--- EdgeDetection-Sample/MainWindow.xaml.cs
+++ EdgeDetection-Sample/MainWindow.xaml.cs
@@ -1,18 +1,19 @@
-﻿using System;
+﻿using Microsoft.Win32;
+
+using OpenCvSharp;
+
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
-using Microsoft.Win32;
-
-using OpenCvSharp;
-
-using static System.Math;
 using static OpenCvSharp.Cv2;
 using static OpenCvSharp.Mat;
+using static OpenCvSharp.MatType;
+using static System.Math;
+
 
 namespace EdgeDetection_Sample {
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -29,6 +30,13 @@ namespace EdgeDetection_Sample {
         }
 
         #endregion Ctr
+
+        #region type
+
+        private static readonly MatType DoubleOne = CV_64FC1;
+        private static readonly MatType ByteOne = CV_8UC1;
+
+        #endregion
 
         #region Prop
 
@@ -93,71 +101,80 @@ namespace EdgeDetection_Sample {
             this.OutputMat = this.MatConvertFunc.Invoke();
         }
 
-        private void EdgeDetection_OnClick(Object sender, RoutedEventArgs e) {
-            unsafe Mat Marr(Mat src, Int32 kernelDiameter, Double sigma) {
-                var size = kernelDiameter / 2;
-                Mat kernel = new(kernelDiameter, kernelDiameter, MatType.CV_64FC1);
+        private unsafe Mat Marr(Mat src, Int32 kernelDiameter, Double sigma) {
+            var size = kernelDiameter / 2;
+            Mat kernel = new(kernelDiameter, kernelDiameter, DoubleOne);
 
-                var pSrc = (Byte*)src.Ptr().ToPointer();
-                var pKernel = (Double*)kernel.Ptr().ToPointer();
+            var pSrc = (Byte*)src.Ptr().ToPointer();
+            var pKernel = (Double*)kernel.Ptr().ToPointer();
 
-                for (var i = -size; i <= size; i++) {
-                   for (var j = -size; j <= size; j++) {
-                       pKernel[j + size + 2 * size * (i + size)] = 
-                           Exp(-((Pow(j, 2) + Pow(i, 2)) / (Pow(sigma, 2) * 2))) * ((Pow(j, 2) + Pow(i, 2) - 2 * Pow(sigma, 2)) / (2 * Pow(sigma, 4)));
-                   }
+            for (var i = -size; i <= size; i++) {
+                for (var j = -size; j <= size; j++) {
+                    pKernel[j + size + 2 * size * (i + size)] =
+                        Exp(-((Pow(j, 2) + Pow(i, 2)) / (Pow(sigma, 2) * 2))) * ((Pow(j, 2) + Pow(i, 2) - 2 * Pow(sigma, 2)) / (2 * Pow(sigma, 4)));
                 }
+            }
 
-                Mat laplacianMat = Zeros(src.Size(), MatType.CV_64FC1);
-                Mat dst = Zeros(laplacianMat.Size(), MatType.CV_8UC1);
+#if SLOW_MODE
 
-                Filter2D(src, laplacianMat, MatType.CV_64FC1, kernel);
+                Mat laplacianMat = new(src.Rows - size * 2, src.Cols - size * 2, DoubleOne);
+                Mat dst = Zeros(src.Rows - size * 2, src.Cols - size * 2, ByteOne);
                 var pLaplacian = (Double*)laplacianMat.Ptr().ToPointer();
-
-                //Mat laplacianMat = new(src.Rows - size * 2, src.Cols - size * 2, MatType.CV_64FC1);
-                //Mat dst = Zeros(src.Rows - size * 2, src.Cols - size * 2, MatType.CV_8UC1);
-                //var pLaplacian = (Double*)laplacianMat.Ptr().ToPointer();
-                //for (var i = size; i < src.Rows - size; i++) {
-                //    for (var j = size; j < src.Cols - size; j++) {
-                //        Double sum = 0;
-                //        for (var x = -size; x <= size; x++) {
-                //            for (var y = -size; y <= size; y++) {
-                //                sum += pKernel[y + size + (x + size) * 2 * size] * pSrc[j + y + (i + x) * src.Cols];
-                //            }
-                //        }
-                //        pLaplacian[j - size + (i - size) * (src.Cols - 2 * size)] = sum;
-                //    }
-                //}
-
-                var pDst = (Byte*)dst.Ptr().ToPointer();
-                for (var i = 1; i < dst.Rows - 1; i++) {
-                    for (var j = 1; j < dst.Cols - 1; j++) {
-                        if (pLaplacian[j + (i - 1) * dst.Cols] * pLaplacian[j + (i + 1) * dst.Cols] < 0 ||
-                            pLaplacian[j - 1 + (i - 1) * dst.Cols] * pLaplacian[j + 1 + (i + 1) * dst.Cols] < 0 ||
-                            pLaplacian[j + 1 + (i - 1) * dst.Cols] * pLaplacian[j - 1 + (i + 1) * dst.Cols] < 0 ||
-                            pLaplacian[j + 1 + i * dst.Cols] * pLaplacian[j - 1 + i * dst.Cols] < 0) {
-                            pDst[j + i * dst.Cols] = Byte.MaxValue;
+                for (var i = size; i < src.Rows - size; i++)
+                {
+                    for (var j = size; j < src.Cols - size; j++)
+                    {
+                        Double sum = 0;
+                        for (var x = -size; x <= size; x++)
+                        {
+                            for (var y = -size; y <= size; y++)
+                            {
+                                sum += pKernel[y + size + (x + size) * 2 * size] * pSrc[j + y + (i + x) * src.Cols];
+                            }
                         }
+                        pLaplacian[j - size + (i - size) * (src.Cols - 2 * size)] = sum;
                     }
                 }
 
-                return dst;
-            }
+#else // SLOW_MODE
 
-            Mat ProcessMat(Action<Mat> processAction) {
-                Mat[] srcs = this.InputMat.CvtColor(ColorConversionCodes.BGR2HLS).Split();
-                using Mat dst = new();
-                processAction.Invoke(srcs[1]);
-                Merge(srcs, dst);
-                Mat resultMat = dst.CvtColor(ColorConversionCodes.HLS2BGR);
-                foreach (var src in srcs) {
-                    src.Dispose();
+            Mat laplacianMat = Zeros(src.Size(), DoubleOne);
+            Mat dst = Zeros(laplacianMat.Size(), ByteOne);
+            Filter2D(src, laplacianMat, DoubleOne, kernel);
+            var pLaplacian = (Double*)laplacianMat.Ptr().ToPointer();
+
+#endif // SLOW_MODE
+
+            var pDst = (Byte*)dst.Ptr().ToPointer();
+            for (var i = 1; i < dst.Rows - 1; i++) {
+                for (var j = 1; j < dst.Cols - 1; j++) {
+                    if (pLaplacian[j + (i - 1) * dst.Cols] * pLaplacian[j + (i + 1) * dst.Cols] < 0 ||
+                        pLaplacian[j - 1 + (i - 1) * dst.Cols] * pLaplacian[j + 1 + (i + 1) * dst.Cols] < 0 ||
+                        pLaplacian[j + 1 + (i - 1) * dst.Cols] * pLaplacian[j - 1 + (i + 1) * dst.Cols] < 0 ||
+                        pLaplacian[j + 1 + i * dst.Cols] * pLaplacian[j - 1 + i * dst.Cols] < 0) {
+                        pDst[j + i * dst.Cols] = Byte.MaxValue;
+                    }
                 }
-                return resultMat;
             }
 
+            return dst;
+        }
+
+        private Mat ProcessMat(Action<Mat> processAction) {
+            Mat[] srcs = this.InputMat.CvtColor(ColorConversionCodes.BGR2HLS).Split();
+            using Mat dst = new();
+            processAction.Invoke(srcs[1]);
+            Merge(srcs, dst);
+            Mat resultMat = dst.CvtColor(ColorConversionCodes.HLS2BGR);
+            foreach (var src in srcs) {
+                src.Dispose();
+            }
+            return resultMat;
+        }
+
+        private void EdgeDetection_OnClick(Object sender, RoutedEventArgs e) {
             var senderMenuItem = sender as MenuItem;
-            this.activeFunction_TextBlock.Text = senderMenuItem?.Name;
+            this.activeFunction_TextBlock.Text = (senderMenuItem?.Header as String)?[1..^0];
             this.MatConvertFunc = (senderMenuItem?.Tag as String) switch {
                 "MG" => () => this.InputMat.MorphologyEx(MorphTypes.Gradient, null),
                 "S" => () => ProcessMat(src => Sobel(src, src, src.Type(), 1, 1)),
